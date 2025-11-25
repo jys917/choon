cc290e1eb75e7bf64b7f4a00294192fa88427507
User <user@example.com>
2025-11-25 23:39:59 +0900

    Chore: Force redeploy for DB sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // Force redeploy
 import clsx from 'clsx';
 import { Header } from './components/Header';
 import { CalendarView } from './components/Calendar/CalendarView';
